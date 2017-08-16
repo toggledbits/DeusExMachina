bbafa27c1129b3826b67879be4814425d621e8b6
Patrick Rigney <patrick@toggledbits.com>
2017-08-16 10:30:27 -0400

    Correct some issues reported by jshint.com. I have no idea if UI5 is really still working, I dont have it to test with at the moment, and I know a lot of functionality is missing here.

--- J_DeusExMachinaII1.js
+++ J_DeusExMachinaII1.js
@@ -6,25 +6,25 @@ var checkTime;
     var serviceId = "urn:toggledbits-com:serviceId:DeusExMachinaII1";
     var controlled;
     var deusDevice;
-    
+
     configureDeus = function(thisDeusDevice) {
         deusDevice = thisDeusDevice;
         var devices = jsonp.ud.devices;
         var html = "<p>Select the lights to be controlled when enabled.</p>";
-        
+
         function isLight(device) {
             switch(device.device_type) {
             case "urn:schemas-upnp-org:device:BinaryLight:1":
                 return true;
-                
+
             case "urn:schemas-upnp-org:device:DimmableLight:1":
                 return true;
-                
+
             default:
                 return false;
             }
         }
-        
+
         function getControlled() {
             var list = get_device_state(deusDevice, serviceId, "Devices", 0);
             if (typeof(list) == "undefined" || list.match(/^\s*$/)) {
@@ -34,7 +34,7 @@ var checkTime;
             return res;
         }
         controlled = getControlled();
-        
+
         for (var i=0; i<devices.length; i++) {
             if (isLight(devices[i])) {
                 html += "<input type=\"checkbox\"";
@@ -51,26 +51,26 @@ var checkTime;
         if (!isNaN(timeMins)) {
             time = timeMinsToStr(timeMins);
         }
-         
+
         html += "<br />";
         html += "<p>Enter the time (after sunset) to begin shutting off lights</p>";
         html += "<input type=\"text\" onChange=\"checkTime()\" id=\"deusExTime\" /> (HH:MM)";
         set_panel_html(html);
         jQuery("#deusExTime").val(time);
     };
-    
+
     updateDeusControl = function(deviceId) {
         var index = jQuery.inArray(deviceId, controlled);
         if (index >= 0) {
             controlled.splice(index, 1);
-            
+
         } else {
             controlled.push(deviceId);
         }
-        
+
         set_device_state(deusDevice, serviceId, "Devices", controlled.join(','), 0);
-    }
-    
+    };
+
     function timeMinsToStr(totalMinutes) {
         var hours = Math.floor(totalMinutes / 60);
         if (hours < 10) {
@@ -82,11 +82,11 @@ var checkTime;
         }
         return hours+":"+minutes;
     }
-    
+
     function updateTime(timeMins) {
         set_device_state(deusDevice, serviceId, "LightsOut", timeMins, 0);
     }
-    
+
     checkTime = function() {
         var time = jQuery("#deusExTime").val();
         var re = new RegExp("^([0-2][0-9]):([0-6][0-9])$");
@@ -100,5 +100,5 @@ var checkTime;
             }
         }
         alert("Time must be in the format HH:MM (i.e. 22:30)");
-    }
+    };
 })();
